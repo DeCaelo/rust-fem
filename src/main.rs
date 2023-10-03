a6c10cf56aaf905cd5b970386a247622b8835e70
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-03 10:09:43 +0200

    Custom Trait

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 mod shapes;
 
-use crate::shapes::{area::Area, circle::Circle, rect::Rect};
+use crate::shapes::{area::Area, circle::Circle, collisions::Collidable, rect::Rect};
 
 fn main() {
     let rect = Rect::default();
-    let circ = Circle::default();
+    let rect2 = Rect::default();
+
+    let circle = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 1.0,
+    };
+
+    let circle2 = Circle {
+        x: 1.5,
+        y: 1.5,
+        radius: 4.0,
+    };
+
+    rect.collide(&rect2);
+    circle.collide(&circle2);
+    rect.collide(&circle);
 
     println!("{}", rect);
-    println!("{}", circ);
 }
